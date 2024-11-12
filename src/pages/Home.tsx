47abad3ab3ed190dd8651f32cfec841893dee014
BurgerFox1 <jeloaguilar1@gmail.com>
2024-11-12 23:21:18 +0800

    unfinished changes home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,8 +11,30 @@ const HomePage: React.FC = () => {
 
     return (
         <div>
-            <h1 className="text-red-800">This is the home page</h1>
-        
+            <div className="relative flex items-center justify-center min-h-screen bg-custom-bg bg-cover bg-center text-center">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+            
+            {/* Content */}
+            <div className="relative z-10 p-8">
+                <h1 className="text-9xl font-bold text-white mb-4">Calaguas Island</h1>
+                <p className="text-lg text-gray-300 mb-8">
+                Vinzons, Camarines Norte
+                </p>
+            </div>
+            </div>
+            <div className="relative flex items-center justify-center min-h-screen bg-custom-bg bg-cover bg-center text-center">
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+            
+            {/* Content */}
+            <div className="relative z-10 p-8">
+                <h1 className="text-9xl font-bold text-white mb-4">Calaguas Island</h1>
+                <p className="text-lg text-gray-300 mb-8">
+                Vinzons, Camarines Norte
+                </p>
+            </div>
+            </div>
         </div>
     );
 }
