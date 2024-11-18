3b667f1b5bcd2f6d9d4bafc48fa9bcedf1ac396d
siji1007 <christianjohnibanez@gmail.com>
2024-11-19 01:43:26 +0800

    separate the connection file

---
+++ flask-backend/venv/connection.py
@@ -0,0 +1,9 @@
+# connection.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_app(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/tourism_ecommerce'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
